979b60a854eb5604c1084022dc7ed224f71c2715
TamerlanWebd <zanshugurov07@gmail.com>
2025-12-21 14:21:36 +0500

    feat: добавление метода hasRole для проверки ролей пользователя

--- app/Models/User.php
+++ app/Models/User.php
@@ -87,4 +87,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(UserCollection::class);
     }
+
+    public function hasRole(string $roleName): bool
+    {
+        return $this->roles()->where('name', $roleName)->exists();
+    }
 }
