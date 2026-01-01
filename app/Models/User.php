21b4a27e377583156bff04a48e68abdecd61f7c5
TamerlanWebd <zanshugurov07@gmail.com>
2026-01-02 01:17:21 +0500

    refactor: улучшить форматирование и структуру модели User для повышения читаемости

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,96 +1,96 @@
-    <?php
+<?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Foundation\Auth\User as Authenticatable;
-    use Illuminate\Notifications\Notifiable;
-    use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-    class User extends Authenticatable
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    'avatar',
+        'bio',
+        'custom_status',
+        'is_online',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'is_online' => 'boolean',
+    ];
+
+    public function roles()
     {
-        use HasApiTokens, HasFactory, Notifiable;
+        return $this->belongsToMany(Role::class, 'role_user');
+    }
 
-        protected $fillable = [
-            'name',
-            'email',
-            'password',
-        'avatar',
-            'bio',
-            'custom_status',
-            'is_online',
-        ];
-
-        protected $hidden = [
-            'password',
-            'remember_token',
-        ];
-
-        protected $casts = [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'is_online' => 'boolean',
-        ];
-
-        public function roles()
-        {
-            return $this->belongsToMany(Role::class, 'role_user');
-        }
-
-        public function comments()
-        {
-            return $this->hasMany(Comment::class);
-        }
-
-        public function ratings()
-        {
-            return $this->hasMany(Rating::class);
-        }
-
-        public function favorites()
-        {
-            return $this->hasMany(Favorite::class);
-        }
-
-        public function watchHistory()
-        {
-            return $this->hasMany(WatchHistory::class);
-        }
-
-    public function animeList()
+    public function comments()
     {
-        return $this->belongsToMany(Anime::class, 'anime_user')
-            ->withPivot(['status', 'episodes_watched'])
+        return $this->hasMany(Comment::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function watchHistory()
+    {
+        return $this->hasMany(WatchHistory::class);
+    }
+
+public function animeList()
+{
+    return $this->belongsToMany(Anime::class, 'anime_user')
+        ->withPivot(['status', 'episodes_watched'])
+        ->withTimestamps();
+}
+
+
+    public function friends()
+    {
+        return $this->belongsToMany(User::class, 'friendships', 'user_id', 'friend_id')
+            ->wherePivot('status', 'accepted')
             ->withTimestamps();
     }
 
-
-        public function friends()
-        {
-            return $this->belongsToMany(User::class, 'friendships', 'user_id', 'friend_id')
-                ->wherePivot('status', 'accepted')
-                ->withTimestamps();
-        }
-
-        public function friendRequests()
-        {
-            return $this->belongsToMany(User::class, 'friendships', 'friend_id', 'user_id')
-                ->wherePivot('status', 'pending')
-                ->withTimestamps();
-        }
-
-        public function videos()
-        {
-            return $this->hasMany(UserVideo::class);
-        }
-
-        public function collections()
-        {
-            return $this->hasMany(UserCollection::class);
-        }
-
-        public function hasRole(string $roleName): bool
-        {
-            return $this->roles()->where('name', $roleName)->exists();
-        }
+    public function friendRequests()
+    {
+        return $this->belongsToMany(User::class, 'friendships', 'friend_id', 'user_id')
+            ->wherePivot('status', 'pending')
+            ->withTimestamps();
     }
+
+    public function videos()
+    {
+        return $this->hasMany(UserVideo::class);
+    }
+
+    public function collections()
+    {
+        return $this->hasMany(UserCollection::class);
+    }
+
+    public function hasRole(string $roleName): bool
+    {
+        return $this->roles()->where('name', $roleName)->exists();
+    }
+}
