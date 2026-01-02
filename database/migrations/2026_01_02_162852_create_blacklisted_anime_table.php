80ab4fcacc914d7e1080e4a7288cfed808758a89
TamerlanWebd <zanshugurov07@gmail.com>
2026-01-02 21:29:17 +0500

    feat: добавить миграцию для создания таблицы blacklisted_anime

---
+++ database/migrations/2026_01_02_162852_create_blacklisted_anime_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('blacklisted_anime', function (Blueprint $table) {
+            $table->id();
+            $table->string('external_id')->index();
+            $table->string('external_source')->default('anilist');
+            $table->timestamps();
+
+            // Уникальный индекс, чтобы один и тот же ID не дублировался
+            $table->unique(['external_id', 'external_source']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('blacklisted_anime');
+    }
+};
